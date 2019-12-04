2c4b63d5058fa9ded223b09684d83ac45cdcd7d4
EdwardLim78 <weichun78@hotmail.com>
2019-12-04 11:08:43 +0800

    Updates

---
+++ precache-manifest.00de06028e224a6ac3771dbdc344e84b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "635f7aeb4ca9ef604fac0fac4f971aee",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "26dafc4245b14a37f593",
+    "url": "/robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "181f23fbd5395149767c",
+    "url": "/robofriends/static/css/main.69a1f6d3.chunk.css"
+  },
+  {
+    "revision": "26dafc4245b14a37f593",
+    "url": "/robofriends/static/js/2.1f39b60e.chunk.js"
+  },
+  {
+    "revision": "181f23fbd5395149767c",
+    "url": "/robofriends/static/js/main.63d49d17.chunk.js"
+  },
+  {
+    "revision": "7b4f849e958ac4107787",
+    "url": "/robofriends/static/js/runtime-main.a1071882.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
